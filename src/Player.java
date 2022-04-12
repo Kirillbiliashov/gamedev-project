c1b7666cccfa962b8a67d689f049fab44707b053
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-04-12 11:02:15 +0300

    state variables and corresponding methods modified

--- src/Player.java
+++ src/Player.java
@@ -6,8 +6,7 @@ public class Player {
   private Card[] hand = new Card[2];
   private boolean isBB = false;
   private boolean isSB = false;
-  private boolean isButton = false;
-
+  private boolean folded = false;
   public Player(int balance, String nickname) {
     this.balance = balance;
     this.nickname = nickname;
@@ -29,22 +28,23 @@ public class Player {
     return this.hand;
   }
 
-  public void setButton() {
-    this.isButton = true;
-    this.isBB = false;
-    this.isSB = false;
-  }
-
   public void setBB() {
     this.isBB = true;
-    this.isButton = false;
     this.isSB = false;
   }
 
   public void setSB() {
     this.isSB = true;
     this.isBB = false;
-    this.isButton = false;
   }
 
+  public void changeBalance(int delta) {
+    balance += delta;
+  }
+  public void setFolded() {
+    this.folded = true;
+  }
+  public boolean didFold() {
+    return this.folded;
+  }
 }
