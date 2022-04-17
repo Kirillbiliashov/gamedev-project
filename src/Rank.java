8529125fc2e8da5c3b54263dcd97b3d0917435a9
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-04-17 16:29:40 +0300

    int values associated with enum values added

--- src/Rank.java
+++ src/Rank.java
@@ -1,3 +1,22 @@
 public enum Rank {
-  DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+  DEUCE(1),
+  THREE(2),
+  FOUR(3),
+  FIVE(4),
+  SIX(5),
+  SEVEN(6),
+  EIGHT(7),
+  NINE(8),
+  TEN(9),
+  JACK(10),
+  QUEEN(11),
+  KING(12),
+  ACE(13);
+  private final int value;
+  private Rank(int cardValue) {
+    this.value = cardValue;
+  }
+  public int getValue() {
+    return this.value;
+  }
 }
--- src/Suit.java
+++ src/Suit.java
@@ -1,3 +1,13 @@
 public enum Suit {
-  CLUBS, DIAMONDS, SPADES, HEARTS
+  CLUBS(1),
+  DIAMONDS(2),
+  SPADES(3),
+  HEARTS(4);
+  private int value;
+  private Suit(final int suitValue) {
+    this.value = suitValue;
+  }
+  public int getValue() {
+    return this.value;
+  }
 }
