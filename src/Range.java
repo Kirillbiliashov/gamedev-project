ebb2652ed4e38bb1c20d1a2c05788ee8db36f4ac
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-04-30 15:49:31 +0300

    new helper class was added

---
+++ src/Range.java
@@ -0,0 +1,11 @@
+public final class Range {
+  private int start;
+  private int end;
+  public Range(final int start, final int end) {
+    this.start = start;
+    this.end = end;
+  }
+  public boolean contains(final int num) {
+    return this.start <= num && num <= this.end;
+  }
+}
