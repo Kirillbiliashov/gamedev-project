cbef46c7c203876700c90f626f2beadd0ba03dd3
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-05-09 18:53:07 +0300

    accidentally removed entry class was restored

---
+++ project/Demo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import src.GameSession;
+
+ public final class Demo {
+   public static void main(String[] args) {
+     final Scanner input = new Scanner(System.in);
+     System.out.print("Enter your nickname: ");
+     final String nickname = input.nextLine();
+     int balance;
+     do {
+       System.out.print("Enter your balance: ");
+       try {
+         balance = Integer.parseInt(input.nextLine());
+       } catch (Exception e) {
+         balance = 10000;
+       }
+     } while (balance < GameSession.MIN_BALANCE || balance > GameSession.MAX_BALANCE);
+     new GameSession().start(balance, nickname);
+   }
+ }
